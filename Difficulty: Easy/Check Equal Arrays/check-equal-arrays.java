c569861a5e5b6a357f9709da029c4581ed451c55
Sai-Sathwik2718 <158552902+Sai-Sathwik2718@users.noreply.github.com>
2025-12-31 12:13:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean checkEqual(int[] a, int[] b) {
+        // code here
+        if (a.length != b.length) return false;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int x : a)
+            map.put(x, map.getOrDefault(x, 0) + 1);
+
+        for (int x : b) {
+            if (!map.containsKey(x) || map.get(x) == 0)
+                return false;
+            map.put(x, map.get(x) - 1);
+        }
+
+        return true;
+    }
+}
